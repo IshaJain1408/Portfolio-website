be119b5e7e002e0a0f51d48d7df67ce95585286d
IshaJain1408 <116328120+IshaJain1408@users.noreply.github.com>
2024-03-28 04:18:34 +0530

    First Commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Isha Jain</span>  Experienced in ReactJS, Angular, and machine learning  <br><br> through internships at Codextra Protech Pvt Ltd and Goeduhub Technologies. Proficient in C, HTML, CSS, and JavaScript, with a knack for creating visually appealing user interfaces. Skilled in Figma, GitHub, and Scrum methodology. Passionate about enhancing user experiences and driving innovation in web development."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
